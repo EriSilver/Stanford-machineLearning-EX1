9d35a829c55804ae372951c29742944395a25f25
silver <dr.george.erini@gmail.com>
2021-12-25 17:41:50 +0200

    pset1 passed - cost function and gradient descent for one variable

--- gradientDescent.m
+++ gradientDescent.m
@@ -7,6 +7,8 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
+xcolumns = size(X)(1,2); 
+
 for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
@@ -16,12 +18,28 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
+    s = zeros(xcolumns, 1);
 
+    for i = 1 : m,
+        h = 0;
+        for c = 1 : xcolumns,
+            h = h + theta(c, 1) * X(i, c);
+        end;
 
+        drv = h - y(i, 1);
+        for si = 1 : xcolumns,
+            s(si, 1) = s(si, 1) + drv * X(i, si);
+        end;
+    end;
 
+    tmp = zeros(xcolumns, 1);
+    for t = 1 : length(theta),
+        tmp(t, 1) = theta(t, 1) - alpha * s(t, 1) / m;
+    end;
 
-
-
+    for t = 1 : length(theta),
+        theta(t, 1) = tmp(t, 1)
+    end;
 
     % ============================================================
 
